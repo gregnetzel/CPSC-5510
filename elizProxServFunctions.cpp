1844e9fb821626d9fa44872f227fdee62711c575
Elizabeth Phippen <phippene@seattleu.edu>
2017-10-26 15:13:37 -0700

    host, uri, and server request functions

---
+++ elizProxServFunctions.cpp
@@ -0,0 +1,90 @@
+
+bool validRequest(string message);
+string getHost(string message);
+string getRelativeURI(string message, string host);
+string formatRequest(string host, string relURI);
+
+
+int main(int argNum, char* argValues[]){
+	
+	if (argNum != 2){
+		// Incorrect number of arguments
+		cerr << "Incorrect number of arguments. Please try again." << endl;
+		return -1;
+	}
+	
+	//create socket
+	
+	//listen
+	
+	//accept
+	
+	string clientRequest = "";
+	
+	// get client request
+	
+	if(!validRequest(clientRequest)){
+		// send 500 error to client
+	}else{
+		string host = getHost(clientRequest);
+		string relURI = getRelativeURI(clientRequest, host);
+		string serverRequest = formatRequest(host, relURI);
+		
+		// send request to server
+		
+		// get response
+		
+		// send response to client
+	}
+
+	return 0;
+}
+
+// check for valid request
+// GET AbsoluteURI HTTP/1.0
+bool validRequest(string message){
+	// can only process GET messages
+	if(message.find("GET ") != 0){
+		return false;
+	}
+	// must have HTTP/1.0\r\n at end
+	if(message.find(" HTTP/1.0\r\n")+11 != message.length() ||
+		message.find(" HTTP/1.1\r\n")+11 != message.length()){
+		return false;
+	}
+	return true;
+}
+
+// gets host from message
+// http://www.host.com/relURI HTTP
+string getHost(string message){
+	string host = "";
+	string temp = "";
+	
+	// www.host.com/relURI
+	temp.append(message, message.find("://")+3, 
+				message.find(" HTTP") - message.find("://")+3);
+	// www.host.com
+	host.append(temp, 0, temp.find('/'));
+	
+	return host;
+}
+
+string getRelativeURI(string message, string host){
+	string relURI = "";
+	
+	relURI.append(message, message.find(host)+host.length(), 
+				message.find(" HTTP") - message.find(host)+host.length());
+	
+	return relURI;
+}
+
+// formats server request
+string formatRequest(string host, string relURI){
+	
+	string response = "GET " + relURI + " HTTP\r\n" +
+		"Host: " + host + "\r\n\r\n";
+		
+	return response;
+}
+
