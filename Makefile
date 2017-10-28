ce30fd2b65374ca3142aaf482b64b8771c09006b
Greg Netzel <greg.8535@gmail.com>
2017-10-28 12:09:16 -0700

    Fixed make and added send added required headers
    Make was making the wrong file, program runs but recv connections aren't
    happy

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 proxyMake: proxyserver.cpp
 	g++ -pthread -o proxy proxyserver.cpp
+	g++ ProxyServerPhase1.cpp -o proxyPhaseOne
--- ProxyServerPhase1.cpp
+++ ProxyServerPhase1.cpp
@@ -10,11 +10,11 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <string>
+#include <iostream>
 using namespace std;
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once
-
-#define PORT "10250"  // the port users will be connecting to
 
 #define BACKLOG 30     // how many pending connections queue will hold
 
@@ -33,7 +33,7 @@ void send_message(int newfd, string msg, int msgLength);
 
 int main(int argNum, char* argValues[])
 {
-	int host_sock_fdesc, newSock_fdesc;  // listen on sock_fd, new connection on new_fd
+	int host_sock_fdesc, newSock_fdesc, server_fdesc;  // listen on sock_fd, new connection on new_fd, server fd
 	struct addrinfo host_info, *host_info_list, *socket_bind;
 	struct sockaddr_storage clientRequest_addr; // connector's address information
 	socklen_t sin_size;
@@ -48,45 +48,40 @@ int main(int argNum, char* argValues[])
 		cerr << "Incorrect number of arguments. Please try again." << endl;
 		return -1;
 	}
-
 	//learn server address and port
 	memset(&host_info, 0, sizeof host_info);
 	host_info.ai_family = AF_UNSPEC;
 	host_info.ai_socktype = SOCK_STREAM;
 	host_info.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, PORT, &host_info, &host_info_list)) != 0)
-	{
-			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-			return 1;
+	if ((rv = getaddrinfo(NULL, argValues[1], &host_info, &host_info_list)) != 0){
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
 	}
 
 	//prepare the socket: create and bind
-	socket_bind = create_and_bind_socket(host_info_list, host_sock_fdesc, yes)
+	socket_bind = create_and_bind_socket(host_info_list, host_sock_fdesc, yes);
 
 	freeaddrinfo(host_info_list); // all done with this structure
 
-	if (socket_bind == NULL)
-	{
-			fprintf(stderr, "server: failed to bind\n");
-			exit(1);
+	if (socket_bind == NULL){
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
 	}
 
 	//listen
-	if (listen(host_sock_fdesc, BACKLOG) == -1)
-	{
-			perror("listen");
-			exit(1);
+	if (listen(host_sock_fdesc, BACKLOG) == -1){
+		perror("listen");
+		exit(1);
 	}
 
 	// reap all dead processes
 	sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1)
-	{
-			perror("sigaction");
-			exit(1);
+	if (sigaction(SIGCHLD, &sa, NULL) == -1){
+		perror("sigaction");
+		exit(1);
 	}
 
 	printf("server: waiting for connections...\n");
@@ -94,32 +89,43 @@ int main(int argNum, char* argValues[])
 	//accept
 	sin_size = sizeof clientRequest_addr;
 	newSock_fdesc = accept(host_sock_fdesc, (struct sockaddr *)&clientRequest_addr, &sin_size);
-	if (newSock_fdesc == -1)
-	{
-			perror("accept");
-			continue;
+	if (newSock_fdesc == -1){
+		perror("accept");
+		return 1;
 	}
 	print_success_client_IP(clientRequest_addr);
 
 	// get client request
 	// client_Message = recv_message(sockfd);
-	clientRequest = recv_message(host_sock_fdesc);
+	clientRequest = recv_message(newSock_fdesc);
 
 	if(!validRequest(clientRequest)){
 		// send 500 error to client
-		send_message(host_sock_fdesc, "Error 500", 9);
+		send_message(newSock_fdesc, "Error 500", 9);
 	}else{
 		string host = getHost(clientRequest);
 		string relURI = getRelativeURI(clientRequest, host);
 		string serverRequest = formatRequest(host, relURI);
 
 		// send request to server
-
+		memset(&host_info, 0, sizeof host_info);
+		host_info.ai_family = AF_UNSPEC;
+		host_info.ai_socktype = SOCK_STREAM;
+		if ((rv = getaddrinfo(host.c_str(), argValues[1], &host_info, &host_info_list)) != 0){
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+			return 1;
+		}
+		socket_bind = create_and_bind_socket(host_info_list, server_fdesc, yes);
+		freeaddrinfo(host_info_list);
+		send_message(server_fdesc, serverRequest, serverRequest.length());
 		// get response
-
+		string serverResponse = recv_message(server_fdesc);
+		close(server_fdesc);
 		// send response to client
+		send_message(newSock_fdesc, serverResponse, serverResponse.length());
+		close(newSock_fdesc);		
 	}
-
+	close(host_sock_fdesc);
 	return 0;
 }
 
@@ -168,16 +174,13 @@ string getRelativeURI(string message, string host){
 
 // formats server request
 string formatRequest(string host, string relURI){
-
 	string response = "GET " + relURI + " HTTP\r\n" +
 		"Host: " + host + "\r\n\r\n";
-
 	return response;
 }
 
 // get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
+void *get_in_addr(struct sockaddr *sa){
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
@@ -186,43 +189,32 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 //proxy creates a socket and binds to local address and port number
-struct addrinfo* create_and_bind_socket(struct addrinfo *servinfo_list, int& sockfd, int& yes)
-{
-  struct addrinfo* servinfo;
-
+struct addrinfo* create_and_bind_socket(struct addrinfo *servinfo_list, int& sockfd, int& yes){
+	struct addrinfo* servinfo;
 	// loop through all the results and bind to the first we can
 	for(servinfo = servinfo_list; servinfo != NULL;
-				servinfo = servinfo->ai_next)
-	{
-			if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype,
-							servinfo->ai_protocol)) == -1)
-			{
-					perror("server: socket");
-					continue;
-			}
-
-			if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-							sizeof(int)) == -1)
-			{
-					perror("setsockopt");
-					exit(1);
-			}
-
-			if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
-			{
-					close(sockfd);
-					perror("server: bind");
-					continue;
-			}
-
-			break;
+			servinfo = servinfo->ai_next){
+		if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype,
+						servinfo->ai_protocol)) == -1){
+			perror("server: socket");
+			continue;
+		}
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+						sizeof(int)) == -1){
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
+				close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+		break;
 	}
-
-  return servinfo;
+	return servinfo;
 }
 
-void sigchld_handler(int s)
-{
+void sigchld_handler(int s){
     // waitpid() might overwrite errno, so we save and restore it:
     int saved_errno = errno;
 
@@ -232,8 +224,7 @@ void sigchld_handler(int s)
 }
 
 //prints IP address of successfully connected client to screen
-void print_success_client_IP(struct sockaddr_storage &client_addr)
-{
+void print_success_client_IP(struct sockaddr_storage &client_addr){
   char s[INET6_ADDRSTRLEN];
 
   inet_ntop(client_addr.ss_family,
@@ -243,20 +234,17 @@ void print_success_client_IP(struct sockaddr_storage &client_addr)
 }
 
 //uses a loop to receive message from client, returns a message as string
-string recv_message(int sock_fd)
-{
+string recv_message(int sock_fd){
   char buf[MAXDATASIZE];
   int num_bytes_recv = 0;
   int checkbytes = 0;
   string clientMessage = "";
 
-  while((num_bytes_recv = recv(sock_fd, buf, MAXDATASIZE-1, 0)) > 0)
-  {
+  while((num_bytes_recv = recv(sock_fd, buf, MAXDATASIZE-1, 0)) > 0){
     checkbytes += num_bytes_recv;
   }
 
-  if (num_bytes_recv == -1)
-  {
+  if (num_bytes_recv == -1){
       perror("recv");
       exit(1);
   }
@@ -269,13 +257,11 @@ string recv_message(int sock_fd)
 }
 
 //sends message to client
-void send_message(int newfd, string msg, int msgLength)
-{
+void send_message(int newfd, string msg, int msgLength){
   int num_bytes_send = 0;
   int checkbytes = msgLength;
 
-  while(checkbytes > num_bytes_send)
-  {
+  while(checkbytes > num_bytes_send)  {
     num_bytes_send = send(newfd, &msg, msgLength, 0);
     checkbytes -= num_bytes_send;
   }
